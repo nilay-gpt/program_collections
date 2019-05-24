c53b87e6c0b31754d7e092a3ae128da5c67b8888
nilay <nilaygpt@gmail.com>
2019-05-24 13:46:18 +0530

    Update find_if_right_rotaion.py

--- find_if_right_rotaion.py
+++ find_if_right_rotaion.py
@@ -2,8 +2,6 @@
 Given a string s1 and a string s2, write a snippet to say whether s2 is a rotation of s1?
 (eg given s1 = ABCD and s2 = CDAB, return true, given s1 = ABCD, and s2 = ACBD , return false)
 """
-
-
 class IfRightRotation():
     def __init__(self, s1, s2):
         self.s1 = s1
@@ -11,9 +9,9 @@ class IfRightRotation():
         self.super_string = self.s1 + self.s1
 
     def check_if_right_rotation(self):
-        if len(self.s1) != len(s2):
+        if len(self.s1) != len(self.s2):
             return False
-        return True if self.super_string.count(s2) > 0 else False
+        return True if self.super_string.count(self.s2) > 0 else False
 
 
 if __name__=="__main__":
